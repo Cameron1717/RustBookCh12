95dcf66b595f21a94d7073542e5ae68debfe538e
Anise <cameron.casebolt17@gmail.com>
2025-08-10 12:00:27 -0700

    added more detailed error hamdling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use mini_grep;
 use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    let config = mini_grep::Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments:{err}");
+        process::exit(1);
+    });
 }
